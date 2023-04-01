5dcf213b8742fbac632141dcf08b07a920aced9f
mezeijanos <janosmezei16@gmail.com>
2023-04-01 14:37:42 +0200

    Update GYAK05.py

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -9,43 +9,7 @@ class KNNClassifier:
     def __init__(self, k:int, test_split_ratio:float):
         self.k = k
         self.test_split_ratio = test_split_ratio
-        self.x_train = 0
-        self.y_train = 0
-        self.x_test = 0
-        self.y_test = 0
 
-    def train_test_split(self, features:np.ndarray,
-                     labels:np.ndarray) -> None:
-        
-        test_size = int(len(features) * self.test_split_ratio)
-        train_size = len(features) - test_size
-        assert len(features) == test_size + train_size, "Size mismatch!"
-
-        self.x_train, self.y_train = features[:train_size,:],labels[:train_size]
-        self.x_test, self.y_test = features[train_size:train_size+test_size,:], labels[train_size:train_size + test_size]
-
-
-    def euclidean(self, element_of_x:np.ndarray) -> np.ndarray:
-        return np.sqrt(np.sum((self.x_train - element_of_x)**2,axis=1))
-
-
-    def predict(self, x_test:np.ndarray) -> np.ndarray:
-        labels_pred = []
-        for x_test_element in x_test:
-            distances = self.euclidean(self.x_train,x_test_element)
-            distances = np.array(sorted(zip(distances,self.y_train)))
-            label_pred = mode(distances[:self.k,1],keepdims=False).mode
-            labels_pred.append(label_pred)
-        self.y_preds = np.array(labels_pred,dtype=np.int32)
-
-    def accuracy(self) -> float:
-        true_positive = (self.y_test == self.y_preds).sum()
-        return true_positive / len(self.y_test) * 100
-
-
-    def confusion_matrix(self):
-        conf_matrix = confusion_matrix(self.y_test,self.y_preds)
-        return conf_matrix
 
     #property for returning k value from constructor
     @property
@@ -62,5 +26,43 @@ class KNNClassifier:
         return x,y
 
 
+    def train_test_split(self, features:np.ndarray,
+                     labels:np.ndarray) -> None:
+        
+        test_size = int(len(features) * self.test_split_ratio)
+        train_size = len(features) - test_size
+        assert len(features) == test_size + train_size, "Size mismatch!"
 
-    
+        x_train, y_train = features[:train_size,:],labels[:train_size]
+        x_test, y_test = features[train_size:train_size+test_size,:], labels[train_size:train_size + test_size]
+
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
+
+
+    def euclidean(self, element_of_x:np.ndarray) -> np.ndarray:
+        return np.sqrt(np.sum((self.x_train - element_of_x)**2,axis=1))
+
+
+    def predict(self, x_test:np.ndarray) -> np.ndarray:
+        labels_pred = []
+        for x_test_element in x_test:
+            distances = self.euclidean(x_test_element)
+            distances = np.array(sorted(zip(distances,self.y_train)))
+
+            label_pred = mode(distances[:self.k,1],keepdims=False).mode
+            labels_pred.append(label_pred)
+
+        self.y_preds = np.array(labels_pred,dtype=np.int32)
+
+    def accuracy(self) -> float:
+        true_positive = (self.y_test == self.y_preds).sum()
+        return true_positive / len(self.y_test) * 100
+
+
+    def confusion_matrix(self):
+        conf_matrix = confusion_matrix(self.y_test,self.y_preds)
+        return conf_matrix
+
